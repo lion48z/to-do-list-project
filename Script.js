7db222373d6dac2e8a12df5580248218995585d8
lion48z <140863653+lion48z@users.noreply.github.com>
2023-08-22 18:18:30 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,37 +1,38 @@
-//task list Script.js
+// todo-list Script.js
 var form = document.getElementById('todo-form');
 var input = document.getElementById('todo-input');
-var input = document.getElementById('todo-list');
+var list = document.getElementById('todo-list');
 
-//load tasks from user input
-window.addEventListener ('load', function() {
-    for (var i = 0; i < localStorage.length; i++) { 
-    addTaskToList(localStorage.key(i));   
-}
+// Event listener local storage
+window.addEventListener('load', function() {
+  for (var i = 0; i < localStorage.length; i++) {
+    addTaskToList(localStorage.key(i));
+  }
 });
+//event lister submit bar
 
 form.addEventListener('submit', function(event) {
-    event.preventDefault();
- //input and submit tasks  
-   
-    var task = input.value;
-    input.value = '';
-   
-  //adds task to to do list  
-    addTaskToList(task);
-    localStorage.setItem(task, task);
-   });
-   
-   function addTaskToList(task) {
-    var listItem = document.createElement('li');
-    listItem.textContent = task;
-    listItem.addEventListener('click', function() {
-      listItem.remove();
-      localStorage.removeItem(task);
-    });
-  
-    list.appendChild(listItem);
-  }
+  event.preventDefault();
+
+  var task = input.value;
+  input.value = '';
+
+  addTaskToList(task);
+  localStorage.setItem(task, task);
+});
+
+//creates task list
+function addTaskToList(task) {
+  var listItem = document.createElement('li');
+  listItem.textContent = task;
+  listItem.addEventListener('click', function() {
+    listItem.remove();
+    localStorage.removeItem(task);
+  });
+
+  list.appendChild(listItem);
+}
+
   
 
        
