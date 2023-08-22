013f88819f747d05bedd0388b97783fb1c74dec2
lion48z <140863653+lion48z@users.noreply.github.com>
2023-08-22 17:38:52 -0500

    Update Script.js

---
+++ Script.js
@@ -0,0 +1,10 @@
+//task list Script.js
+var form = document.getElementById('todo-form');
+var input = document.getElementById('todo-input');
+var input = document.getElementById('todo-list');
+
+//load tasks from user input
+window.addEventListener('load', function() {
+    console.log('Page has loaded!');
+});
+
